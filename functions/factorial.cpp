d8facb2d03662c2deb95ef851f4e59b3c7d9c98d
K S Antony Micheal <starkantony356@gmail.com>
2021-09-24 10:18:18 +0530

    functions folder is created

---
+++ functions/factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    int f = 1;
+    for(int i =2;i<=n;i++){
+        f=f*i;
+    }
+    return f;
+}
+int main(){
+    int n;
+    cin>>n;
+    int fact =  factorial(n);
+    cout<<"factorial of "<<n<<" ="<<fact;
+    return 0;
+}
