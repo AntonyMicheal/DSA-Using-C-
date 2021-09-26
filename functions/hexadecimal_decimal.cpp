76c2bf8e9fe5ae2b514fbd0c5b54d0cb9ad9e569
K S Antony Micheal <starkantony356@gmail.com>
2021-09-26 14:47:25 +0530

    Hexa decimal to decimal added

--- functions/hexadecimal_decimal.cpp
+++ functions/hexadecimal_decimal.cpp
@@ -1,8 +1,22 @@
 //hexadecimal_decimal
 #include<iostream>
 using namespace std;
-string hex_decimal(string n)
+int hex_decimal(string n)
 {
+    int size = n.size();
+    int decimal = 0;
+    int base = 1;
+    while(size>0){
+        if(n[size-1]>='A' && n[size-1]<='F'){
+            decimal+= base * (n[size-1] - 'A'+10);
+        }
+        else if(n[size-1]>='0' && n[size-1]<='9')  {
+            decimal+= base * (n[size-1] -'0');
+        }
+        base*=16;
+        size --;
+    }
+    return decimal;
 
 }
 
