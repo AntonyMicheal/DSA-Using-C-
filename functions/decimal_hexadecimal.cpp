7535fbd138368b9dbf1c6cfd846900beacb9089f
K S Antony Micheal <starkantony356@gmail.com>
2021-10-06 20:14:17 +0530

    re visited some old problems

--- functions/decimal_hexadecimal.cpp
+++ functions/decimal_hexadecimal.cpp
@@ -1,28 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string dec_hex(int s){
-    string hexa = "";
+// string dec_hex(int s){
+//     string hexa = "";
+//     int base = 1;
+//     while(base<=s){
+//         base*=16;
+//     }
+//     base/=16;
+
+//     while(base>0){
+//         int last_digit = s/base;
+//         s-= last_digit*base;
+//         base/=16;
+//         if(last_digit<=9){
+//             hexa += to_string(last_digit);
+//         }
+//         else{
+//             char c = 'A'+last_digit - 10;
+//             hexa.push_back(c);
+//         }
+//     }
+//     return hexa;
+// }
+string dec_hex(int n){
+    string s ="";
     int base = 1;
-    while(base<=s){
+    
+    while(base<=n){
         base*=16;
     }
     base/=16;
-
     while(base>0){
-        int last_digit = s/base;
-        s-= last_digit*base;
+
+        int rem = n/base;
+        n-=rem*base;
         base/=16;
-        if(last_digit<=9){
-            hexa += to_string(last_digit);
+        if(rem<=9){
+            s+= to_string(rem);
         }
         else{
-            char c = 'A'+last_digit - 10;
-            hexa.push_back(c);
+            char c = 'A'+rem - 10;
+            s.push_back(c);
         }
+        
     }
-    return hexa;
+    return s;
 }
+
+
 int main(){
     int s;
     cin>>s;
