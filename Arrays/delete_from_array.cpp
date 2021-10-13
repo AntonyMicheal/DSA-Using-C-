fa8af458ea819c654d97a6008e7df4f59e4d7e65
K S Antony Micheal <starkantony356@gmail.com>
2021-10-14 00:01:55 +0530

    updated delete array

--- Arrays/delete_from_array.cpp
+++ Arrays/delete_from_array.cpp
@@ -1,24 +1,27 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
 int main(){
-    int n;
-    cout<<"Enter size of array: ";
+    int n,k;
     cin>>n;
     int array[n];
-    cout<<"Enter array elements: ";
     for(int i=0;i<n;i++){
         cin>>array[i];
     }
-    int index;
-    cout<<"Enter index:";
-    cin>>index;
-    for(int i=index;i<n-1;i++){
-        array[i] = array[i+1];
+    cout<<"\n";
+    cin>>k;
+
+
+    for(int i=0;i<n;i++){
+        if(array[i]==k){
+            for(int j=i;j<n;j++){
+                array[j] = array[j+1];
+            }
+        }
     }
 
     for(int i=0;i<n-1;i++){
-        cout<<array[i]<<" ";
+        cout<<" "<<array[i];
     }
+    
     return 0;
 }
